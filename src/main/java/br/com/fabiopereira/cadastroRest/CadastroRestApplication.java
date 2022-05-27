c3185513a1338e47338f1eeada261ac66146c10b
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2022-05-27 14:20:29 -0300

    upload inicial

---
+++ src/main/java/br/com/fabiopereira/cadastroRest/CadastroRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabiopereira.cadastroRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroRestApplication.class, args);
+	}
+
+}
